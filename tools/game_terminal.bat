5f0e517ee03abba3ac88ecef39be4b5e11ae43ef
achoum <achoum@gmail.com>
2022-08-25 22:11:33 +0200

    Working on maps

--- tools/game_terminal.bat
+++ tools/game_terminal.bat
@@ -9,4 +9,4 @@ bazel build %MODE% //exploratron/cli:game %FLAG%
 if %errorlevel% neq 0 exit /b %errorlevel%
 echo "====================================="
 
-bazel-bin\exploratron\cli\game "--map=robot_city.tmx"
+bazel-bin\exploratron\cli\game "--map=robot_city_2.tmx"
